0f185dfd25dff3cdc982bcd555fae9e2856b63fa
Rumesh Eranga <rehrumesh@hotmail.com>
2013-09-22 00:25:26 +0530

    init

---
+++ USBLocker/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace USBLocker
+{
+    partial class frmUSBLocker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValue = new System.Windows.Forms.Label();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(12, 9);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 13);
+            this.lblValue.TabIndex = 0;
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(12, 41);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(101, 23);
+            this.btnDo.TabIndex = 1;
+            this.btnDo.Text = "Enable/Dsable";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Developed By Rumesh Eranga";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmUSBLocker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 71);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.lblValue);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmUSBLocker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "USB Locker";
+            this.Load += new System.EventHandler(this.frmUSBLocker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ USBLocker/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace USBLocker
+{
+    public partial class frmUSBLocker : Form
+    {
+        RegistryKey rkey;
+
+        public frmUSBLocker()
+        {
+            InitializeComponent();
+            rkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\USBSTOR", true);
+            int val = int.Parse(rkey.GetValue("Start").ToString());
+            if (val == 3)
+            {
+                lblValue.Text = "USB Ports enabled";
+            }
+            else
+            {
+                lblValue.Text = "USB Ports disabled";
+            }
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                int val = int.Parse(rkey.GetValue("Start").ToString());
+                //lblValue.Text = ""+val;
+                if (val == 3)
+                {
+                    rkey.SetValue("Start", 4);
+                    lblValue.Text = "USB Ports disabled";
+                }
+                else {
+                    rkey.SetValue("Start", 3);
+                    lblValue.Text = "USB Ports enabled";
+                }
+                //rkey.SetValue("Start", 4);
+            }catch(Exception ex){
+                string s = ex.Message;
+                lblValue.Text = "Program settings has been reseted.Please run application as administrator";
+            }
+        }
+
+        private void frmUSBLocker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rumesh Eranga\nrehrumesh@hotmail.com\nUniversity of Colombo School of Computing", "About");
+        }
+
+        
+    }
+}
